3392eba1b5003ffa1c68c8db66b8242bc20ca6c5
Minh Dao <minhdao6@gmail.com>
2014-07-16 11:17:15 +0700

    added Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by minh on 7/16/14.
+ * Program starts here
+ * Threads and sockets for servers are created here
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
